14af48b6ddeaf86fe2c6409bb30937dbaa40a41b
manyi <zhaomanyi2005@gmail.com>
2023-04-17 22:51:08 -0700

    as per our mech lead's request

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/drivebase/BallDrivebase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/drivebase/BallDrivebase.java
@@ -7,4 +7,13 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class BallDrivebase {
     public BallDrivebase(HardwareMap hardwareMap, Telemetry telemetry) {
     }
+
+    // For Lillian's Ego
+    public String compliment(int repeats){
+        String comp = "";
+        for (int i = 0; i < repeats; i++)
+            comp += "Lillian's balldrive is so pretty\n";
+
+        return comp;
+    }
 }
