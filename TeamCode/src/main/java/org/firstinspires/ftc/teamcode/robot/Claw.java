87cd466708d35b82f7384ba80e84e80111884e91
Ivan <ivanxu1116@gmail.com>
2023-06-13 16:26:45 -0700

    did claw code!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Claw.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.robot;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -7,8 +8,22 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Claw {
 
-    private final double MAX_POSITION = 1;
-    private final double MIN_POSITION = 0;
+    private final double CLAW_UP = 1;
+    private final double CLAW_DOWN = 0;
+    Servo leftClaw;
+    Servo rightClaw;
 
+    public Claw (HardwareMap hardwareMap, Telemetry telemetry) {
+        leftClaw = hardwareMap.get(Servo.class, "leftClaw");
+        rightClaw = hardwareMap.get(Servo.class, "rightClaw");
+    }
 
+    public void moveClawUp () {
+        leftClaw.setPosition(CLAW_UP);
+        rightClaw.setPosition(CLAW_UP);
+    }
+    public void moveClawDown () {
+        leftClaw.setPosition(CLAW_DOWN);
+        rightClaw.setPosition(CLAW_DOWN);
+    }
 }
