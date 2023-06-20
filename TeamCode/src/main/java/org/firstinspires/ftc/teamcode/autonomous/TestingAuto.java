de9883213dfe98dc880d39815206340026537d5d
manyi <zhaomanyi2005@gmail.com>
2023-06-19 17:53:36 -0700

    testing move

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TestingAuto.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.actions.AutoActions;
+
+@Autonomous
+public class TestingAuto extends LinearOpMode {
+    private AutoActions actions;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        actions = new AutoActions(hardwareMap, telemetry, AutoActions.fieldSide.LEFT);
+
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+
+        actions.initRobot();
+
+        while (opModeIsActive()){
+            telemetry.addData("zone", actions.cam.getZone());
+
+            actions.getTelemetry();
+            telemetry.update();
+        }
+        actions.cam.closeCamera();
+    }
+}
